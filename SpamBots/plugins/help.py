ffd47f6bb1bd16f4e42ae822947c2f4fc0bbfec8
opbrutal <93695693+opbrutal@users.noreply.github.com>
2022-03-23 10:52:26 +0530

    Update help.py

--- SpamBots/plugins/help.py
+++ SpamBots/plugins/help.py
@@ -25,7 +25,7 @@ UstaD_Help += f" `.raid` - to raid\n `.replyraid` - to active reply raid\n `.dre
 UstaD_Help += f"© @Nishu_bothub\n"
 
 
-@.on(events.NewMessage(incoming=True, pattern=r"/help))
+@.on(events.NewMessage(incoming=True, pattern=r"/help"))
 async def help(event):               
     if event.sender_id in SUDO_USERS:
       await UstaD.send_file(event.chat_id,
