cad7fdb9b78ee50c9c6f73767044ff564f5ddee2
opbrutal <93695693+opbrutal@users.noreply.github.com>
2022-03-23 10:24:58 +0530

    Update brothers.py

--- SpamBots/plugins/brothers.py
+++ SpamBots/plugins/brothers.py
@@ -5,7 +5,6 @@ from telethon import events
 from telethon import functions, types
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from .. import UstaD, UstaD2, UstaD3, UstaD4, UstaD5, UstaD6, UstaD7, UstaD8, UstaD9, UstaD10, SUDO_USERS
-from SpamBots import CMD_HNDLR as hl
 from SpamBots import GROUP, PORMS
 
 
